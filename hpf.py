d292a27c3ad4b5be4ad5101c0da8319f369f0c67
omarsgalal <omarsgalal4@gmail.com>
2018-11-05 20:44:16 +0200

    fixing hpf and round robin

--- hpf.py
+++ hpf.py
@@ -1,5 +1,5 @@
 from scheduler import Scheduler
-
+from plotter import ProcessesPlotter
 
 class HPF(Scheduler):
 	def __init__(self):
@@ -21,18 +21,16 @@ class HPF(Scheduler):
 
 		max_priority = self.get_max_priority()
 
-		self.current_processes.append(self.processes[0])
-		self.current_processes[0].priority = max_priority - self.current_processes[0].priority
+		i = 0
 
-		i = 1
-		while i < num_processes and self.processes[i].arrival == self.processes[i-1].arrival:
-			self.current_processes.append(self.processes[i])
-			self.current_processes[-1].priority = max_priority - self.current_processes[-1].priority
-			i += 1
+		self.passed_time += self.processes[i].arrival
 
-		self.passed_time += self.current_processes[0].arrival
+		while i < num_processes or len(self.current_processes) > 0:
 
-		while len(self.current_processes) > 0:
+			while i < num_processes and self.processes[i].arrival <= self.passed_time:
+				self.current_processes.append(self.processes[i])
+				self.current_processes[-1].priority = max_priority - self.current_processes[-1].priority
+				i += 1
 
 			self.current_processes.sort(key = lambda x: x.id)
 			self.current_processes.sort(key = lambda x: x.priority)
@@ -43,13 +41,9 @@ class HPF(Scheduler):
 			final_processes[-1].priority = max_priority - final_processes[-1].priority
 			self.current_processes = self.current_processes[1:]
 
-			if len(self.current_processes) == 0 and i < num_processes:
-				self.passed_time = self.processes[i].arrival
 
-			while i < num_processes and self.processes[i].arrival <= self.passed_time:
-				self.current_processes.append(self.processes[i])
-				self.current_processes[-1].priority = max_priority - self.current_processes[-1].priority
-				i += 1
+			if len(self.current_processes) == 0 and i < num_processes and self.passed_time < self.processes[i].arrival:
+				self.passed_time = self.processes[i].arrival
 
 
 
@@ -61,6 +55,14 @@ if __name__ == "__main__":
 	hpf.read_processes()
 	processes = hpf.schedule()
 	outfile = open('scheduled_processes.txt', 'w')
+	x = []
+	y = []
 	for p in processes:
 		outfile.write(str(p))
+		x.append(p.end - p.burst)
+		y.append(p.id)
+		x.append(p.end)
+		y.append(p.id)
+	plotter = ProcessesPlotter()
+	plotter.plot(x, y)
 	outfile.close()
